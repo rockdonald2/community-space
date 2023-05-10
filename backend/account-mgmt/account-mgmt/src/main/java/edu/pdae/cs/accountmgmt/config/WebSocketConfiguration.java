fb9a5cf105c62e960440a778ce9aa727ce3ca82d
Lukács Zsolt <rockdonald2@gmail.com>
2023-05-10 23:06:30 +0300

    Configure autostartup

--- backend/account-mgmt/account-mgmt/src/main/java/edu/pdae/cs/accountmgmt/config/WebSocketConfiguration.java
+++ backend/account-mgmt/account-mgmt/src/main/java/edu/pdae/cs/accountmgmt/config/WebSocketConfiguration.java
@@ -3,11 +3,14 @@ package edu.pdae.cs.accountmgmt.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocket
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
@@ -28,7 +31,8 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/account").setAllowedOrigins(corsOrigins).withSockJS(); // this is the WS endpoint
+        registry.addEndpoint("/stomp/account").setAllowedOrigins(corsOrigins);
+        registry.addEndpoint("/stomp/account").setAllowedOrigins(corsOrigins).withSockJS();
     }
 
     @Override
@@ -38,7 +42,8 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
                 .setRelayHost(relayHost)
                 .setRelayPort(relayPort)
                 .setClientLogin(relayUser)
-                .setClientPasscode(relayPwd);
+                .setClientPasscode(relayPwd)
+                .setAutoStartup(true);
     }
 
 }
