2b981ebfcb47c3527d147bc603f1a32f3c9df345
Lukács Zsolt <rockdonald2@gmail.com>
2023-06-02 11:36:47 +0300

    Added tags and pushes to deploy.sh

--- devops/ci/deploy.sh
+++ devops/ci/deploy.sh
@@ -9,6 +9,8 @@ echo
 
 cd account-mgmt/account-mgmt || exit 1
 docker build -t account-mgmt .
+docker tag account-mgmt:latest registry.gitlab.com/rockdonald2/community-space:account-mgmt-latest
+docker push registry.gitlab.com/rockdonald2/community-space:account-mgmt-latest
 cd ../..
 
 # Deploy gateway
@@ -18,6 +20,8 @@ echo
 
 cd gateway/gateway || exit 1
 docker build -t gateway .
+docker tag gateway:latest registry.gitlab.com/rockdonald2/community-space:gateway-latest
+docker push registry.gitlab.com/rockdonald2/community-space:gateway-latest
 cd ../..
 
 # Deploy hub-mgmt
@@ -27,6 +31,8 @@ echo
 
 cd hub-mgmt/hub-mgmt || exit 1
 docker build -t hub-mgmt .
+docker tag hub-mgmt:latest registry.gitlab.com/rockdonald2/community-space:hub-mgmt-latest
+docker push registry.gitlab.com/rockdonald2/community-space:hub-mgmt-latest
 cd ../..
 
 # Deploy memo-mgmt
@@ -36,6 +42,8 @@ echo
 
 cd memo-mgmt/memo-mgmt || exit 1
 docker build -t memo-mgmt .
+docker tag memo-mgmt:latest registry.gitlab.com/rockdonald2/community-space:memo-mgmt-latest
+docker push registry.gitlab.com/rockdonald2/community-space:memo-mgmt-latest
 cd ../..
 
 # Deploy notification-mgmt
@@ -45,6 +53,8 @@ echo
 
 cd activity-notifications-mgmt/activity-notifications-mgmt || exit 1
 docker build -t activity-notifications-mgmt .
+docker tag activity-notifications:latest registry.gitlab.com/rockdonald2/community-space:activity-notifications-latest
+docker push registry.gitlab.com/rockdonald2/community-space:activity-notifications-latest
 cd ../..
 
 # Deploy front-end
@@ -54,6 +64,8 @@ echo
 
 cd ../frontend/community-space || exit 1
 docker build -t community-space .
+docker tag community-space:latest registry.gitlab.com/rockdonald2/community-space:web-latest
+docker push registry.gitlab.com/rockdonald2/community-space:web-latest
 # shellcheck disable=SC2103
 cd ..
 
